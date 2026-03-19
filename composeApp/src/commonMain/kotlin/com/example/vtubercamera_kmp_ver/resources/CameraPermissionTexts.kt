c3e6859908cb1f61b20d13fcf2d478ff46c9ecc5
jtakumi <takumi87jn@gmail.com>
2026-03-19 15:19:08 +0900

    CameraPermissionTexts brige iOS string resource

---
+++ composeApp/src/commonMain/kotlin/com/example/vtubercamera_kmp_ver/resources/CameraPermissionTexts.kt
@@ -0,0 +1,32 @@
+package com.example.vtubercamera_kmp_ver.resources
+
+import org.jetbrains.compose.resources.getString
+import vtubercamera_kmp_ver.composeapp.generated.resources.Res
+import vtubercamera_kmp_ver.composeapp.generated.resources.camera_permission_checking_description
+import vtubercamera_kmp_ver.composeapp.generated.resources.camera_permission_denied_description
+import vtubercamera_kmp_ver.composeapp.generated.resources.camera_permission_launch_button
+import vtubercamera_kmp_ver.composeapp.generated.resources.camera_permission_not_determined_description
+import vtubercamera_kmp_ver.composeapp.generated.resources.camera_permission_open_settings_button
+import vtubercamera_kmp_ver.composeapp.generated.resources.camera_permission_required_message
+
+class CameraPermissionTexts(
+    val requiredMessage: String,
+    val deniedDescription: String,
+    val notDeterminedDescription: String,
+    val checkingDescription: String,
+    val openSettingsButtonTitle: String,
+    val launchCameraButtonTitle: String,
+)
+
+class CameraPermissionTextsLoader {
+    suspend fun load(): CameraPermissionTexts {
+        return CameraPermissionTexts(
+            requiredMessage = getString(Res.string.camera_permission_required_message),
+            deniedDescription = getString(Res.string.camera_permission_denied_description),
+            notDeterminedDescription = getString(Res.string.camera_permission_not_determined_description),
+            checkingDescription = getString(Res.string.camera_permission_checking_description),
+            openSettingsButtonTitle = getString(Res.string.camera_permission_open_settings_button),
+            launchCameraButtonTitle = getString(Res.string.camera_permission_launch_button),
+        )
+    }
+}
