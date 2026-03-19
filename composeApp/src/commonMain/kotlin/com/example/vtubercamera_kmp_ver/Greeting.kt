8cade29ff486389f878b0813b1b208a9994c78a5
jtakumi <takumi87jn@gmail.com>
2026-03-19 13:55:06 +0900

    delete default kotlin files

--- composeApp/src/commonMain/kotlin/com/example/vtubercamera_kmp_ver/Greeting.kt
+++
@@ -1,9 +0,0 @@
-package com.example.vtubercamera_kmp_ver
-
-class Greeting {
-    private val platform = getPlatform()
-
-    fun greet(): String {
-        return "Hello, ${platform.name}!"
-    }
-}
