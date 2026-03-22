060e5a9093836522127568e6f614d6c78f867a15
jtakumi <takumi87jn@gmail.com>
2026-03-22 20:43:51 +0900

    change function name

--- composeApp/src/commonMain/kotlin/com/example/vtubercamera_kmp_ver/App.kt
+++ composeApp/src/commonMain/kotlin/com/example/vtubercamera_kmp_ver/App.kt
@@ -3,12 +3,12 @@ package com.example.vtubercamera_kmp_ver
 import androidx.compose.material3.MaterialTheme
 import androidx.compose.runtime.Composable
 import androidx.compose.ui.tooling.preview.Preview
-import com.example.vtubercamera_kmp_ver.camera.CameraScreen
+import com.example.vtubercamera_kmp_ver.camera.CameraRoute
 
 @Composable
 @Preview
 fun App() {
     MaterialTheme {
-        CameraScreen()
+        CameraRoute()
     }
 }
