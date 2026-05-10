bfecf1aa7193b8ea7407848067f396b2a558fd43
jtakumi <takumi87jn@gmail.com>
2026-05-11 00:07:41 +0900

    Expose zoom state in camera repository

--- composeApp/src/commonMain/kotlin/com/example/vtubercamera_kmp_ver/camera/CameraSharedDefinitions.kt
+++ composeApp/src/commonMain/kotlin/com/example/vtubercamera_kmp_ver/camera/CameraSharedDefinitions.kt
@@ -79,6 +79,10 @@ interface CameraRepository {
     fun onPlatformPreviewStarted(lensFacing: CameraLensFacing)
 
     fun onPlatformPreviewError(lensFacing: CameraLensFacing, error: CameraError)
+
+    fun observeZoomState(): Flow<CameraZoomUiState>
+
+    fun onPlatformZoomStateChanged(zoomUiState: CameraZoomUiState)
 }
 
 interface PermissionRepository {
